79649e61bdb7ca97d60c25c6ae21851694dab333
Jiyi <jiyixx@gmail.com>
2015-02-24 16:26:50 +0800

    26.3 创建处理网络连接的专用类，并调用测试

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
@@ -1,14 +1,20 @@
 package com.bignerdranch.android.photogallery;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
 
+import java.io.IOException;
+
 public class PhotoGalleryFragment extends Fragment{
+
+    private static final String TAG = "PhotoGalleryFragment";
 
     GridView mGridView;
 
@@ -17,6 +23,7 @@ public class PhotoGalleryFragment extends Fragment{
         super.onCreate(savedInstanceState);
 
         setRetainInstance(true);
+        new FetchItemsTask().execute();
     }
 
     @Override
@@ -26,5 +33,18 @@ public class PhotoGalleryFragment extends Fragment{
         mGridView = (GridView)v.findViewById(R.id.gridView);
 
         return v;
+    }
+
+    private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                String result = new FlickrFetchr().getUrl("http://www.google.com");
+                Log.i(TAG, "Fetched contents of URL: " + result);
+            } catch (IOException ioe) {
+                Log.e(TAG, "Failed to fetch URL: ", ioe);
+            }
+            return null;
+        }
     }
 }
